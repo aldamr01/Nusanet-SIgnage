e9fa6fb657174863e489feaa1ae884da31b80ea5
aldamr01 <aldionamirrul@gmail.com>
2019-02-11 11:18:39 +0700

    fixing user screen

--- app/controllers/Signage.php
+++ app/controllers/Signage.php
@@ -27,17 +27,15 @@ class Signage extends CI_Controller
         $site       =   Site::find($site_id);
         $screen     =   Screen_Device::find($screen_id);
         $content    =   Content::where('device_id',$screen_id)->orderBy('created_at','DESC')->get();
-        header('Content-Type:Application/json');
+        //header('Content-Type:Application/json');
         
 
         if($site->token == $token)
         {
             if($screen->site_id == $site->id)
-            {
-                echo json_encode($content);
-                die();
-                $data['content']    =   1;
-
+            {                
+                $data['content']    =   $content;                
+                
                 echo $this->blade->stream('administrator.signage.template3',$data);                                
             }
             else
